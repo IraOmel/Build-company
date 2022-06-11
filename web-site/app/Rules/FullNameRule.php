a660d65a8da13b0a1b90845ba0ea548df7cee757
Ira Omelchenko <81821850+IraOmel@users.noreply.github.com>
2022-06-11 20:09:36 +0300

    Rule for data validation

--- web-site/app/Rules/FullNameRule.php
+++ web-site/app/Rules/FullNameRule.php
@@ -6,23 +6,13 @@ use Illuminate\Contracts\Validation\Rule;
 
 class FullNameRule implements Rule
 {
-    /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
+    
     public function __construct()
     {
         //
     }
 
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param string $attribute
-     * @param mixed $value
-     * @return bool
-     */
+   
     public function passes($attribute, $value)
     {
         if (preg_match('/^[a-zA-Z]+(?:\s[a-zA-Z]+)+$/', $value)
@@ -33,11 +23,7 @@ class FullNameRule implements Rule
         }
     }
 
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
+   
     public function message()
     {
         return 'Прізвище та ім\'я написано неправильно';
