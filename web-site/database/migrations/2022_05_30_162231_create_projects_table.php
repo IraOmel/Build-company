c6d060b60b148a617d9180b8185701bdf7dfc039
Ira Omelchenko <81821850+IraOmel@users.noreply.github.com>
2022-06-11 20:05:31 +0300

    Projects table

--- web-site/database/migrations/2022_05_30_162231_create_projects_table.php
+++ web-site/database/migrations/2022_05_30_162231_create_projects_table.php
@@ -6,11 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+  
     public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
@@ -26,12 +22,6 @@ return new class extends Migration
         });
     }
 
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('projects');
